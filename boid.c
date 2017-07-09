1c4f45af509d01455198b452f2e7ed872cd80d1d
Kevin Berry <kpberry11@gmail.com>
2017-07-09 14:37:01 -0400

    Fixes square viewing radius issue with rules 4 and 5

--- boid.c
+++ boid.c
@@ -146,8 +146,7 @@ void rule3(BOID* cur, BOID* others, int* neighbors) {
 //find food rule; make the boid move toward the food if the food is withing 4
 //times the viewing radius
 void rule4(BOID* cur, FOOD* food) {
-    if (SQR(cur->x - FIX8(food->x)) < SQR(viewRadius)
-        && SQR(cur->y - FIX8(food->y)) < SQR(viewRadius)) {
+    if (SQR(cur->x - FIX8(food->x)) + SQR(cur->y - FIX8(food->y)) < SQR(viewRadius)) {
         cur->vx += UNFIX8((FIX8(food->x) - cur->x) * (cohesion << 2));
         cur->vy += UNFIX8((FIX8(food->y) - cur->y) * (cohesion << 2));
     }
@@ -156,8 +155,7 @@ void rule4(BOID* cur, FOOD* food) {
 //avoid player rule; make the boid move away from the player if the player is
 //within the view radius
 void rule5(BOID* cur, PLAYER* player) {
-    if (SQR(cur->x - FIX8(player->x)) < SQR(viewRadius)
-        && SQR(cur->y - FIX8(player->y)) < SQR(viewRadius)) {
+    if (SQR(cur->x - FIX8(player->x)) + SQR(cur->y - FIX8(player->y)) < SQR(viewRadius)) {
         cur->vx -= UNFIX8((FIX8(player->x) - cur->x) * (repulsion >> 2));
         cur->vy -= UNFIX8((FIX8(player->y) - cur->y) * (repulsion >> 2));
     }
